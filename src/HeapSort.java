8f5cb75fd9402b21cf59ca2a448f27c1face2cb1
Nikolin Majmari <nikolinmajmari30@gmail.com>
2025-05-20 10:46:32 +0200

    solutions with group b

--- src/HeapSort.java
+++ src/HeapSort.java
@@ -3,11 +3,34 @@ import java.util.Arrays;
 public class HeapSort {
 
     public static void sort(int[] arr){
-        /// todo your code here
+        for(int i=arr.length/2-1; i>=0; i--){
+            heapify(arr, arr.length,i);
+        }
+        for (int i = arr.length-1; i > 0; i--) {
+            int temp = arr[0];
+            arr[0] = arr[i];
+            arr[i] = temp;
+            heapify(arr, i, 0);
+        }
     }
 
     private static void heapify(int[] arr, int len,int i) {
-      // todo your code here
+        int left = 2 * i + 1;
+        int right = 2 * i + 2;
+        int largest = i;
+        if (left < len && arr[left] > arr[largest]) {
+            largest = left;
+        }
+        if (right < len && arr[right] > arr[largest]) {
+            largest = right;
+        }
+        if (largest != i) {
+            int temp = arr[i];
+            arr[i] = arr[largest];
+            arr[largest] = temp;
+            printHeapTree(arr,arr.length);
+            heapify(arr, len, largest);
+        }
     }
 
     public static void printHeapTree(int[] arr, int size) {
@@ -41,6 +64,7 @@ public class HeapSort {
         int[] arr = new int[]{
             5,6,11,10,9,12,8,7,2,1,6
         };
+        printHeapTree(arr,arr.length);
         sort(arr);
         printHeapTree(arr,arr.length);
     }
